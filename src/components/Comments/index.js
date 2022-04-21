79e2ecd4400700a8e03a56c359ec81b092bb1485
NenoProgramador <104164084+NenoProgramador@users.noreply.github.com>
2022-04-21 19:20:09 -0300

    Update index.js

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -11,7 +11,7 @@ const Comments = ({ url, title }) => {
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="wadsongarbes"
+        shortname="blog-do-neno"
         identifier={completeURL}
         title={title}
         url={completeURL}
