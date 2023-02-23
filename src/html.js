5a25bd25b6a9e086c7d536c10ee7ea9eab26be07
NenoProgramador <104164084+NenoProgramador@users.noreply.github.com>
2023-02-23 19:14:25 -0300

    Adiciona gtag google analytics

--- src/html.js
+++ src/html.js
@@ -13,6 +13,15 @@ export default function HTML(props) {
         />
         {props.headComponents}
       </head>
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-7T50QTVD3K"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-7T50QTVD3K');
+      </script>
       <body {...props.bodyAttributes} className="dark">
         <script
           dangerouslySetInnerHTML={{
